9dd1870fcaf289c97c3655d9173ddecacdd2b109
kwaw <17128203+amihere@users.noreply.github.com>
2024-11-13 21:51:10 +0000

    init

--- db/utils.go
+++ db/utils.go
@@ -1,28 +1,74 @@
 package db
 
 import (
-  "log"
-	"database/sql"
+	"context"
+	"log"
+	"strings"
 	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/dgraph-io/dgo"
+	"github.com/dgraph-io/dgo/protos/api"
 )
 
+const (
+	dgraphAddress = "127.0.0.1:9180"
+)
+
+var database *dgo.Dgraph
+var cancel CancelFunc
+var lock = sync.Mutex{}
+
+type CancelFunc func()
+
 type Definition struct {
-	db *sql.DB
-	mu sync.Mutex
-  id int
-  name string
-  description string
-  phonetic string
-  refs []int
-  aliases []int
+	id          int
+	name        string
+	description string
+	phonetic    string
+	refs        []int
+	aliases     []int
 }
 
-func AddDefinition(name string) bool {
-  return false
+func NewClient() (*dgo.Dgraph, CancelFunc) {
+	if database == nil {
+		db, _cancel := newClient()
+		database = db
+		cancel = _cancel
+	}
+	return database, cancel
 }
 
-func CheckError(err error) {
-  if err != nil {
-    log.Fatal(err)
-  }
+func newClient() (*dgo.Dgraph, CancelFunc) {
+	conn, err := grpc.NewClient(dgraphAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("While trying to dial gRPC")
+	}
+
+	dc := api.NewDgraphClient(conn)
+	dg := dgo.NewDgraphClient(dc)
+	ctx := context.Background()
+
+	// Perform login call. If the Dgraph cluster does not have ACL and
+	// enterprise features enabled, this call should be skipped.
+	for {
+		// Keep retrying until we succeed or receive a non-retriable error.
+		err = dg.Login(ctx, "groot", "password")
+		if err == nil || !strings.Contains(err.Error(), "Please retry") {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	if err != nil {
+		log.Fatalf("While trying to login %v", err.Error())
+	}
+
+	return dg, func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Error while closing connection:%v", err)
+		}
+	}
 }
--- dictionary/operations.go
+++ dictionary/operations.go
@@ -1,13 +1,27 @@
 package dictionary
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
 	dbUtils "gogeta.io/fante/db"
+	"net/http"
 )
 
 func GetWotd(c *gin.Context) {
-  dbClient, _ := dbUtils.NewClient()
+	db, _ := dbUtils.NewClient()
+
+	findWotd := `query exactMatch($a: Definition) {
+    me(func: eq(name, $a)) {
+      name
+      dgraph.type
+    }
+  }`
+	txn := db.NewReadOnlyTxn()
+
+	variables := make(map[string]string)
+	variables["$a"] = "word"
+
+	txn.QueryWithVars(c, findWotd, variables)
+
 	word := DefinitionModel{2014, "Mentally disposed; willing m\u025Bk\u037B", "egq", "ready"}
 	c.JSON(http.StatusOK, word)
 }
@@ -15,4 +29,3 @@ func GetWotd(c *gin.Context) {
 func Query(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"ready": c.Query("w")})
 }
-
