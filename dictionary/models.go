cc836d3bcf9f7184f7811dc147043fd5f42bd7b0
kwaw <17128203+amihere@users.noreply.github.com>
2024-11-27 23:36:48 +0000

    do not use global meili client

--- dictionary/models.go
+++ dictionary/models.go
@@ -2,33 +2,44 @@
 package dictionary
 
 import (
-	"github.com/meilisearch/meilisearch-go"
 	"database/sql"
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
-  "os"
+	"github.com/meilisearch/meilisearch-go"
 	"log"
+	"os"
 )
 
 // DB is a SQL pool variable
 var DB *sql.DB
 
+// DefinitionModel - model object for word in dictionary
 type DefinitionModel struct {
-	Id          string   `json:"id"`
+	ID          string   `json:"id"`
 	Description string   `json:"description"`
 	Phonetic    string   `json:"phonetic"`
 	Name        string   `json:"name"`
 	Tags        []string `json:"tags"`
 }
 
-type Definition struct {
+type definitionModelIndex struct {
+	ID   string   `json:"id"`
+	Name string   `json:"name"`
+	Tags []string `json:"tags"`
+}
+
+type definition struct {
 	id          int
 	name        string
 	description string
 	phonetic    string
 	refs        []int
 	aliases     []int
+}
+
+func getIndexName() string {
+	return "definition-model"
 }
 
 func (t *DefinitionModel) Scan(value interface{}) error {
@@ -46,11 +57,17 @@ func AddDefinition(name string) bool {
 
 // GetDefinition finds a word using name for fuzzy find
 func GetDefinition(name string) (DefinitionModel, error) {
+	fmt.Println(name)
+	// find the name using meili
+
+	id := "796e9bd273244c4e5edabaad5bfc7b4"
+	return getDefinition(id)
+}
+
+func getDefinition(id string) (DefinitionModel, error) {
 	var def DefinitionModel
 
-	name += "%"
-	fmt.Println(name)
-	err := DB.QueryRow("SELECT definition FROM words WHERE definition->>'name' LIKE ?", name).Scan(&def)
+	err := DB.QueryRow("SELECT definition FROM words WHERE definition->>'id' = ?", id).Scan(&def)
 
 	if err != nil {
 		fmt.Println(err)
@@ -81,30 +98,28 @@ func SetupDatabase(init bool) {
 	failIfErr(err)
 	defer stmt.Close()
 
-	def := DefinitionModel{Id: "796e9bd273244c4e5edabaad5bfc7b4", Name: "ready", Description: "Mentally disposed; willing m\u025Bk\u037B", Phonetic: "re_a_dyia"}
+	def := DefinitionModel{ID: "796e9bd273244c4e5edabaad5bfc7b4", Name: "ready", Description: "Mentally disposed; willing m\u025Bk\u037B", Phonetic: "re_a_dyia"}
 	marshalled, _ := json.Marshal(def)
 	_, err = stmt.Exec(string(marshalled))
 
 	failIfErr(err)
 }
 
+// SetupMeili should init meili driver pool
 func SetupMeili() {
-  client := meilisearch.New("http://localhost:7700", meilisearch.WithAPIKey(os.Getenv("MEILI_KEY")))
+  meiliKey := os.Getenv("MEILI_KEY")
+  search := meilisearch.New("http://localhost:7700", meilisearch.WithAPIKey(meiliKey))
 
 	// An index is where the documents are stored.
-	index := client.Index("movies")
+	index := search.Index(getIndexName())
 
 	// If the index 'movies' does not exist, Meilisearch creates it when you first add the documents.
-	documents := []map[string]interface{}{
-		{"id": 1, "title": "Carol", "genres": []string{"Romance", "Drama"}},
-		{"id": 2, "title": "Wonder Woman", "genres": []string{"Action", "Adventure"}},
-		{"id": 3, "title": "Life of Pi", "genres": []string{"Adventure", "Drama"}},
-		{"id": 4, "title": "Mad Max: Fury Road", "genres": []string{"Adventure", "Science Fiction"}},
-		{"id": 5, "title": "Moana", "genres": []string{"Fantasy", "Action"}},
-		{"id": 6, "title": "Philadelphia", "genres": []string{"Drama"}},
+	documents := []definitionModelIndex{
+		{ID: "796e9bd273244c4e5edabaad5bfc7b4", Name: "ready", Tags: []string{"Fantasy", "Action"}},
+		{ID: "6", Name: "Philadelphia", Tags: []string{"Drama"}},
 	}
 	task, err := index.AddDocuments(documents)
-  failIfErr(err)
+	failIfErr(err)
 
 	fmt.Println(task.TaskUID)
 }
